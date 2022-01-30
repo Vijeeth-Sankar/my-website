69826ed5d5bd48407699c0ef85b40c573610c317
Vijeeth143 <kuttyk321@gmail.com>
2022-01-30 16:11:41 +0530

    dfdsd

--- src/component/First.js
+++ src/component/First.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Navbar, Container, Nav} from 'react-bootstrap';
+import {Navbar, Container, Nav,Offcanvas} from 'react-bootstrap';
 import './Frist.css';
 
 export default class First extends Component {
@@ -7,21 +7,41 @@ export default class First extends Component {
     return (
         <>
         <div id='full'>
-            <Navbar  id='navbar' expand="lg" sticky="top">
-              <Container>
-                <Navbar.Brand id='vij' href="#">Vij</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav" >
-                  <Nav className="mx-auto">
-                    <Nav.Link id='nav-word' href="#let">Service</Nav.Link>
-                    <Nav.Link id='nav-word' href="#line-box">Project</Nav.Link>
-                    <Nav.Link id='nav-word' href="#see">About me</Nav.Link>
-                    <Nav.Link id='nav-word' href="#lins">Contact</Nav.Link>
-                  </Nav>
-                </Navbar.Collapse>
-                <Navbar.Brand id='colla'  href="#">Collaboration</Navbar.Brand>
-              </Container>
-            </Navbar>
+        <Navbar expand="lg" id='desktop'>
+        <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mx-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+            <Navbar.Brand className="ms-auto" href="#home">React-Bootstrap</Navbar.Brand>
+        </Container>
+        </Navbar>
+        <Navbar id='mobile' bg="light" expand={false}>
+        <Container fluid>
+            <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+            <Navbar.Toggle aria-controls="offcanvasNavbar" />
+            <Navbar.Offcanvas
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+            placement="end"
+            >
+            <Offcanvas.Header closeButton>
+                <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="#action1">Home</Nav.Link>
+                <Nav.Link href="#action2">Link</Nav.Link>
+                
+                </Nav>
+            </Offcanvas.Body>
+            </Navbar.Offcanvas>
+        </Container>
+        </Navbar>
         </div>
         </>
     );
